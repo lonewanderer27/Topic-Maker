c5f1383c7aec9fbadeb7cbb1121904baa55c11b9
lonewanderer27 <adrianejames27@gmail.com>
2022-07-16 02:02:18 +0800

    program complete lol

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import os
+from datetime import date
+from keep_alive import keep_alive
 import interactions
 
 
-TESTING_SERVERS=[941233178729938955]
+# TESTING_SERVERS=[941233178729938955, 997532964659404910]
 
-bot = interactions.Client(token=os.environ['TOPIC_MAKERTM_TOKEN'])
+bot = interactions.Client(token=os.environ['ENHANCEDTOPIC_MAKERTM_TOKEN'])
+today = date.today()
 
 
-@bot.command(name="ping", description="ping", scope=TESTING_SERVERS)
+@bot.command(name="ping", description="ping")
 async def ping(ctx: interactions.CommandContext):
+    print('%s has sent a ping, so we pong!' %ctx.user.username)
     await ctx.send("pong!")
 
 
 @bot.command(
     name="thread", 
-    description="Makes a thread of the specified options", 
-    scope=TESTING_SERVERS,
+    description="Makes a thread of the specified options",
     options = [
         interactions.Option(
             name="name",
@@ -25,9 +28,9 @@ async def ping(ctx: interactions.CommandContext):
         ),
         interactions.Option(
             name="role",
-            description="Role to mention in the thread",
+            description="Roles to mention in the thread",
             type=interactions.OptionType.MENTIONABLE,
-            required=True
+            required=True,
         ),
         interactions.Option(
             name="channel",
@@ -37,16 +40,30 @@ async def ping(ctx: interactions.CommandContext):
     ])
 async def thread(ctx: interactions.CommandContext, name: str, role, channel=None):
     if channel == None:
-        print("user did not chose a channel")
         channel = await ctx.get_channel()
+        channel_name = 'this'
     else:
-        print("user chose a channel")
-    print(channel)
-    await ctx.send(channel.mention)
+        channel_name = channel.mention
+
+    thread_created_embed = interactions.Embed(
+        title=name,
+        description="A topic made by %s for %s channel." %(ctx.user.mention, channel_name),
+        thumbnail = interactions.EmbedImageStruct(url=ctx.member.user.avatar_url),
+        color = 0x022af5,
+        footer = interactions.EmbedFooter(text=today.strftime("%B %d, %Y"))
+    )
+
+    if ctx.channel == channel:
+        await ctx.send(embeds=thread_created_embed)
+    else:
+        await ctx.send("%s kindly check your newly created thread in %s channel." %(ctx.user.mention, channel.mention), ephemeral=True)
+        await channel.send(embeds=thread_created_embed)
+
     thread = await channel.create_thread(name=name)
-    await thread.send(f"```{name}```")
-    await thread.send(role.mention)
-    await ctx.send(f"{thread.mention} has been created!")
+    await thread.send(embeds=thread_created_embed)
+
+    print('%s%s: created thread on channel: %s' %(ctx.user.username, "#"+ctx.member.user.discriminator, channel.name))
     
 
+keep_alive()
 bot.start()
